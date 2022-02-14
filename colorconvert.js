207004a2bfd4f68aa21be78a50b805508a1c9ea9
malte <malt0533@stud.kea.dk>
2022-02-14 09:45:01 +0100

    Color convert exercise done

--- colorconvert.js
+++ colorconvert.js
@@ -2,3 +2,14 @@
 "use strict;";
 
 console.log("Hello Color Convert");
+
+let hex = "#c0ffee";
+let converted = colorConvert(hex);
+
+function colorConvert(hex) {
+	r = parseInt(hex.substring(1, 3), 16);
+	g = parseInt(hex.substring(3, 5), 16);
+	b = parseInt(hex.substring(5, 7), 16);
+	return { red: r, green: g, blue: b };
+}
+console.log(converted);
