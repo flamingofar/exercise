48cc1e649c9d02132cd51e67678e39d499ba6529
malte <malt0533@stud.kea.dk>
2022-02-14 10:21:40 +0100

    CSS color convert, rgbtohex, hextorgb done

--- colorconvert.js
+++ colorconvert.js
@@ -3,13 +3,42 @@
 
 console.log("Hello Color Convert");
 
+// HEX to RGB
 let hex = "#c0ffee";
-let converted = colorConvert(hex);
+let convertedHex = colorConvert(hex);
 
-function colorConvert(hex) {
-	r = parseInt(hex.substring(1, 3), 16);
-	g = parseInt(hex.substring(3, 5), 16);
-	b = parseInt(hex.substring(5, 7), 16);
-	return { red: r, green: g, blue: b };
+function colorConvert(hexString) {
+	r = parseInt(hexString.substring(1, 3), 16);
+	g = parseInt(hexString.substring(3, 5), 16);
+	b = parseInt(hexString.substring(5, 7), 16);
+	return { r: r, g: g, b: b };
 }
-console.log(converted);
+console.log(convertedHex);
+
+// RGB TO HEX
+let rgb = { r: 4, g: 119, b: 123 };
+let convertedRGB = rgbToHex(rgb);
+function rgbToHex(rgbObject) {
+	return `#${convertion(rgbObject.r)}${convertion(rgbObject.g)}${convertion(rgbObject.b)}`;
+}
+function convertion(color) {
+	let hex = color.toString(16);
+	return hex.length === 1 ? "0" + hex : hex;
+}
+console.log(convertedRGB);
+
+// CSS to RGB
+let css = "rgb(192, 13, 1)";
+
+function cssToRGB(css) {
+	const rawTxt = css
+		.substring(css.indexOf("(") + 1, css.indexOf(")"))
+		.trim()
+		.split(", ");
+	let r, g, b;
+	r = parseInt(rawTxt[0].trim());
+	g = parseInt(rawTxt[1].trim());
+	b = parseInt(rawTxt[2].trim());
+	console.log(`RGB Values are: r:${r}, g:${g}, b:${b}`);
+}
+cssToRGB(css);
