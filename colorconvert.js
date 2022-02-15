5c429f2798b91c5bab9698d21a767682b2a39cb9
malte <malt0533@stud.kea.dk>
2022-02-15 08:50:00 +0100

    colorconvert cleanup

--- colorconvert.js
+++ colorconvert.js
@@ -11,7 +11,7 @@ function colorConvert(hexString) {
 	r = parseInt(hexString.substring(1, 3), 16);
 	g = parseInt(hexString.substring(3, 5), 16);
 	b = parseInt(hexString.substring(5, 7), 16);
-	return { r: r, g: g, b: b };
+	return { r, g, b };
 }
 console.log(convertedHex);
 
