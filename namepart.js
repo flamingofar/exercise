94e605fce1593bf7cca14aece50941a21235d76b
malte <malt0533@stud.kea.dk>
2022-02-15 08:38:23 +0100

    getNameParts exercise done

--- namepart.js
+++ namepart.js
@@ -2,7 +2,7 @@
 "use strict;";
 const simpleName = "Malte Skjoldager";
 const fullNamePt1 = "Malte Tambo Skjoldager";
-const headmaster = "Albus Percival Wulfric Brian Dumbledore";
+const headmaster = "albus percival wulfric brian dumbledore";
 
 printName(headmaster);
 
@@ -19,9 +19,10 @@ function printName(name) {
 	}
 
 	lastName = name.substring(name.lastIndexOf(" "), name.length).trim();
-	console.table({ firstName, middleName, lastName });
+	// console.table({ firstName, middleName, lastName });
 
-	fullName(lastName, firstName, middleName);
+	// fullName(lastName, firstName, middleName);
+	return { firstName, middleName, lastName };
 }
 
 // Nameparts Pt.2
@@ -40,5 +41,44 @@ function fullName(lastName, firstName, middleName) {
 	return `${firstName.trim()} ${middleName ? middleName + " " : (middleName = "")}${lastName.trim()}`;
 }
 
-let result = fullName("Potter", "Harry", "James", "Pottypotpot");
-console.log("Result", result);
+// let result = fullName("Potter", "Harry", "James", "Pottypotpot");
+
+// Create a function getNameParts( fullName ), that receives a string in any case, splits it into three parts: firstName, middleName and lastName, capitalizes each part correctly, and returns an object with all three parts.
+returnedFullName = getNameParts(headmaster);
+console.table(returnedFullName);
+
+function getNameParts(fullName) {
+	nameObj = printName(fullName);
+	let firstName, middleName, lastName;
+	//Get first name capitalized
+	firstName = capitalization(nameObj.firstName);
+	//Get middle name capitalized
+	middleName = capitalizationMiddleName(nameObj.middleName);
+	//Get last name capitalized
+	lastName = capitalization(nameObj.lastName);
+
+	return { firstName, middleName, lastName };
+}
+
+function capitalization(name) {
+	let firstLetter = name.substring(0, 1).toUpperCase();
+	return `${firstLetter}${name.substring(1).toLowerCase()}`;
+}
+
+function capitalizationMiddleName(middleName) {
+	// Capitalize middle name
+	middleName = capitalization(middleName);
+	// If middlename contains more names, capitalize those
+	// Split into array
+	const textArr = middleName.split("");
+	textArr.forEach((el, idx, arr) => {
+		// If element index contains - or space, element before is uppercased
+		if (el === "-" || el === " ") {
+			arr[idx + 1] = arr[idx + 1].toUpperCase();
+		}
+	});
+	// Joining the array to a string
+	result = textArr.join("");
+	return result;
+}
+capitalizationMiddleName("percival wulfric brian");
