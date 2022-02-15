fb06836e8a05762e1bad85c1ba1a634945a0824e
malte <malt0533@stud.kea.dk>
2022-02-15 07:54:32 +0100

    Capitalization pt.3 done

--- capitalization.js
+++ capitalization.js
@@ -6,10 +6,21 @@ console.log("Capitalization");
 /*
  * Old Exercise
  */
+// let name = "malte";
+// capitalization(name);
+
+// function capitalization(name) {
+// 	let firstLetter = name.substring(0, 1).toUpperCase();
+// 	console.log(`${firstLetter}${name.substring(1).toLowerCase()}`);
+// }
+
+// Capitalize Pt.3
 let name = "malte";
-capitalization(name);
+let result = capitalization(name);
 
 function capitalization(name) {
 	let firstLetter = name.substring(0, 1).toUpperCase();
-	console.log(`${firstLetter}${name.substring(1).toLowerCase()}`);
+	return `${firstLetter}${name.substring(1).toLowerCase()}`;
 }
+
+console.log(result);
