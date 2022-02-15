8de3501a1a16d35501e17faf370d4ddc3ce7c206
malte <malt0533@stud.kea.dk>
2022-02-15 14:37:41 +0100

    Random color exercise done

--- randomColor.js
+++ randomColor.js
@@ -1,10 +1,24 @@
 /** @format */
 "use strict;";
 
-console.log("Hello Basics");
+// console.log("Hello Basics");
+randomBackground();
 
-function randomBackground() {}
+function randomBackground() {
+	const rgbObj = randomColor();
+	const cssColor = rgbToCSS(rgbObj);
 
-function randomColor() {}
+	document.body.style.backgroundColor = cssColor;
+}
 
-function rgbToCSS(rgbObj) {}
+function randomColor() {
+	let r, g, b;
+	r = Math.floor(Math.random() * 255);
+	g = Math.floor(Math.random() * 255);
+	b = Math.floor(Math.random() * 255);
+	return { r, g, b };
+}
+
+function rgbToCSS(rgbObj) {
+	return `rgb(${rgbObj.r}, ${rgbObj.g}, ${rgbObj.b})`;
+}
